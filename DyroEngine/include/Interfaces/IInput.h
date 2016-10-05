38a75e3331ec825e30706ef75f37456604ad3def
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2016-10-05 19:31:44 +0200

    Patch 14:
    Adding the input interface

---
+++ DyroEngine/include/Interfaces/IInput.h
@@ -0,0 +1,15 @@
+#ifndef _IINPUT_H
+#define _IINPUT_H
+
+class Input;
+
+class IInput
+{
+public:
+	IInput(){}
+	virtual ~IInput(){}
+
+	virtual void setupInput(Input* input) = 0;
+};
+
+#endif
---
+++ DyroEngine/src/Interfaces/IInput.cpp
@@ -0,0 +1,3 @@
+#include "Interfaces\IInput.h"
+
+// Nothing to implement
