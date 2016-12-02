d0b26a44178e6b91bc0e3112dc1ef5f379b06ff9
davedebreuck email = davedebreuck@medicim.com <dave.debreuck@medicim.com>
2016-12-02 16:02:39 +0100

    Patch 9: Fix bouding box Adjust BoxCollisionShape Calculation

--- DyroEngine/src/SceneGraph/Component/Collision/BoxCollisionComponent.cpp
+++ DyroEngine/src/SceneGraph/Component/Collision/BoxCollisionComponent.cpp
@@ -1,10 +1,18 @@
 #include "SceneGraph\Component\Collision\BoxCollisionComponent.h"
 #include "SceneGraph\Component\RigidBodyComponent.h"
+#include "SceneGraph\Component\TransformComponent.h"
+
+#include "SceneGraph/Object/GameObjects/PhysicsObject.h"
 
 #include "Defines\programdefaults.h"
 #include "Defines\assert.h"
 
 #include <Box2D\Box2D.h>
+
+namespace
+{
+	const int BOX_COLLISION_VERTICES_SIZE = 4;
+}
 
 BoxCollisionComponent::BoxCollisionComponent(RigidBodyComponent* rigid, float width, float height, float restitution, float friction, float density, const std::tstring& name)
 	:CollisionComponent(rigid, restitution, friction, density, name == _T("") ? _T("BoxCollisionComponent") : name)
@@ -25,8 +33,19 @@ bool BoxCollisionComponent::initialize()
 	this->width /= constants::BOX2D_SCALE; 
 	this->height /= constants::BOX2D_SCALE;
 
+	b2Vec2* vertices = new b2Vec2[BOX_COLLISION_VERTICES_SIZE];
+
+	vertices[0] = (b2Vec2(0.0f, 0.0f));
+	vertices[1] = (b2Vec2(1.0f * width, 0.0f));
+	vertices[2] = (b2Vec2(1.0f * width, 1.0f * height));
+	vertices[3] = (b2Vec2(0.0f, 1.0f * height));
+
+	//TransformComponent* transform = dynamic_cast<PhysicsObject*>(getParent())->getTransform();
+
 	b2PolygonShape shape;
-	shape.SetAsBox(this->width / 2.0f, this->height / 2.0f);
+	//shape.SetAsBox(this->width / 2.0f, this->height / 2.0f, Vector2D::toBox2DVec(transform->getCenterPosition()), transform->getRotation());
+	shape.Set(vertices, BOX_COLLISION_VERTICES_SIZE);
+	shape.m_centroid.SetZero();
 
 	fixtureDef.shape = &shape;
 
--- DyroEngine/src/SceneGraph/Object/Objects/Shapes/PolygonShape.cpp
+++ DyroEngine/src/SceneGraph/Object/Objects/Shapes/PolygonShape.cpp
@@ -38,18 +38,39 @@ bool PolygonShape::getFill() const
 
 Rect2D PolygonShape::getBoundingBox() const
 {
-	std::vector<Vector2D>::const_iterator minIt = std::min_element(this->vec_points.begin(), this->vec_points.end());
-	std::vector<Vector2D>::const_iterator maxIt = std::max_element(this->vec_points.begin(), this->vec_points.end());
+	std::vector<Vector2D>::const_iterator min_x_it = std::min_element(this->vec_points.begin(), this->vec_points.end(),
+		[](const Vector2D& v1, const Vector2D& v2)
+	{
+		return v1.x < v2.x;
+	});
+	std::vector<Vector2D>::const_iterator max_x_it = std::max_element(this->vec_points.begin(), this->vec_points.end(),
+		[](const Vector2D& v1, const Vector2D& v2)
+	{
+		return v1.x < v2.x;
+	});
 
-	Vector2D min = (*minIt);
-	Vector2D max = (*maxIt);
+	std::vector<Vector2D>::const_iterator min_y_it = std::min_element(this->vec_points.begin(), this->vec_points.end(),
+		[](const Vector2D& v1, const Vector2D& v2)
+	{
+		return v1.y < v2.y;
+	});
+	std::vector<Vector2D>::const_iterator max_y_it = std::max_element(this->vec_points.begin(), this->vec_points.end(),
+		[](const Vector2D& v1, const Vector2D& v2)
+	{
+		return v1.y < v2.y;
+	});
+
+	Vector2D min_x = (*min_x_it);
+	Vector2D max_x = (*max_x_it);
+	Vector2D min_y = (*min_y_it);
+	Vector2D max_y = (*max_y_it);
 
 	Rect2D bounding_rect;
 
 	bounding_rect.left = 0;
 	bounding_rect.top = 0;
-	bounding_rect.right = max.x - min.x;
-	bounding_rect.bottom = max.y - min.y;
+	bounding_rect.right = max_x.x - min_x.x;
+	bounding_rect.bottom = max_y.y - min_y.y;
 
 	return bounding_rect;
 }
