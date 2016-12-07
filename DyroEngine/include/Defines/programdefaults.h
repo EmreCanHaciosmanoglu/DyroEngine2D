d5cc38e1ac0c6293a410ceba26d0d9b893da4166
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2016-12-07 07:56:27 +0100

    Patch 16: Adding some more constant values

--- DyroEngine/include/Defines/programdefaults.h
+++ DyroEngine/include/Defines/programdefaults.h
@@ -8,6 +8,11 @@ namespace constants
 	const int BITS_PER_PIXEL_16 = 16;
 
 	const int BOX2D_SCALE = 100;
+
+	const float PI = 3.14159265359f;
+
+	const float TO_RADIANS = PI / 180.0f;
+	const float TO_DEGREE = 180.0f / PI;
 }
 
 #endif //_PROGRAMDEFAULTS_H
