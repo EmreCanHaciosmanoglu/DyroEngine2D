0def96a26ab969e3a01c71d6dc9962231b08705e
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2017-06-23 09:46:03 +0200

    Add more functionality to the safedelete and saferelease

--- DyroEngine/include/Core/Defines/deletemacros.h
+++ DyroEngine/include/Core/Defines/deletemacros.h
@@ -2,7 +2,7 @@
 #define _DELETEMACRO_H
 
 //Defines
-#define SafeRelease(x) { if(x) {x->Release(); x = nullptr;} } // Macro for releasing COM object
-#define SafeDelete(x) { if(x) {delete x; x = nullptr;} } // Macro for deleting objects
+#define SafeRelease(x) { if(x) {x->Release(); x = nullptr;} else {__asm { int 3 };} } // Macro for releasing COM object, if already released trigger breakpoint
+#define SafeDelete(x) { if(x) {delete x; x = nullptr;} else {__asm { int 3 };} } // Macro for deleting objects, if already released trigger breakpoint
 
 #endif
