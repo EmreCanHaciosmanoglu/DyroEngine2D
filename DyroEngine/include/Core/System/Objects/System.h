19b23b99b3615c882b31b70741363fd23f8b57b9
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2017-06-23 10:00:57 +0200

    Make a listener of every system

--- DyroEngine/include/Core/System/Objects/System.h
+++ DyroEngine/include/Core/System/Objects/System.h
@@ -1,9 +1,12 @@
 #ifndef _SYSTEM_H
 #define _SYSTEM_H
 
-enum class SystemType;
+#include "Core/Data/Objects/Message/Listener.h"
 
-class System
+enum class SystemType;
+enum class MessageType;
+
+class System : public Listener
 {
 public:
 	System(SystemType type);
@@ -12,6 +15,9 @@ public:
 	virtual bool initialize() = 0;
 	virtual void update() = 0;
 	virtual bool shutdown() = 0;
+
+	virtual void handleMessage(Message* message) = 0;
+	virtual std::vector<MessageType> listenTo() const = 0;
 
 	void activate();
 	void deactivate();
